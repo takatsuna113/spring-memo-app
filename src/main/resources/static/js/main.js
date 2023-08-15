6c21b1f4c0bb8d8660aa5e8555af8498ca054dad
Takatsuna <takatsuna1113@gmail.com>
2023-08-15 14:24:31 +0900

    完全に削除モーダルを作成

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,18 +1,18 @@
 /** サイドバーのフォーカス切り替え */
-window.onload = function() {
-  const currentUrl = location.href;
-  // activeクラスを追加するクラスを取得
-  const sidebar = document.querySelectorAll('.sidebar-items');
-
-  sidebar.forEach((link) => {
-    // 各コンテンツの詳細リンクのhref属性を取得
-    const activeUrl = link.children[1].children[0].href;
-    // 開いているURLと詳細リンクを押したコンテンツが一致している場合、activeクラスを追加
-    if (currentUrl === activeUrl) {
-      link.classList.add('active');
-    }
-  });
-
+if (location.href.includes('detail')) {
+  window.onload = function () {
+    const currentUrl = location.href;
+    // activeクラスを追加するクラスを取得
+    const sidebar = document.querySelectorAll('.sidebar-items');
+    sidebar.forEach((link) => {
+      // 各コンテンツの詳細リンクのhref属性を取得
+      const activeUrl = link.children[1].children[0].href;
+      // 開いているURLと詳細リンクを押したコンテンツが一致している場合、activeクラスを追加
+      if (currentUrl === activeUrl) {
+        link.classList.add('active');
+      }
+    });
+  }
 }
 
 // 編集・新規作成画面でEnterによるフォーム送信を制御
@@ -26,13 +26,27 @@ if (location.href.includes('edit') || location.href.includes('create')) {
   }
 }
 
-// 
-const modalOpen = () => {
-  const modal = document.querySelector('.confirm');
-  modal.style.visibility = 'visible';
-  modal.style.zIndex = '100';
+// ゴミ箱画面のモーダル表示
+if (location.href.includes('garbage')) {
+  const memoList = document.querySelectorAll('.remove');
+  memoList.forEach((memo) => {
+    memo.addEventListener('click', () => {
+      // 「完全に削除」が押された箇所のメモIDをモーダルに渡す
+      modalOpen(memo.classList[1]);
+    })
+  });
+  // モーダル表示
+  const modalOpen = (memoId) => {    
+    const modal = document.querySelector('.confirm');
+    // href属性(リクエストURL)にidを付与するaタグを取得
+    const rmTargetAnchor = modal.children[0].children[2].children[0];
+    rmTargetAnchor.setAttribute('href', `remove/${memoId}`);
+    modal.style.visibility = 'visible';
+    modal.style.zIndex = '100';
+  }
 }
 
+// モーダル非表示
 const modalClose = () => {
   const modal = document.querySelector('.confirm');
   modal.style.visibility = 'hidden';
